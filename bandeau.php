558ed05cb9253b02169ebd66c0b6fc85df33b1ed
stephane-robin <robin.stephane@outlook.com>
2020-12-02 22:33:31 +0100

    x

---
+++ bandeau.php
@@ -0,0 +1,17 @@
+<div id="bandeau">
+    <h1> Memento </h1>
+    <h1> PHP </h1>
+
+    <nav>
+        <div class="element"> <a href="html.html">HTML</a> </div>
+        <div class="element"> <a href="css.html">CSS</a> </div>
+        <div class="element"> <a href="javascript.html">JAVASCRIPT</a> </div>
+        <div class="element"> <a href="java.html">JAVA</a> </div>
+        <div class="element"> <a href="kotlin.html">KOTLIN</a> </div>
+        <div class="element"> <a href="android.html">ANDROID</a> </div>
+        <div class="element"> <a href="php.html">PHP</a> </div>
+        <div class="element"> <a href="python.html">PYTHON</a> </div>
+        <div class="element"> <a href="c.html">C</a> </div>
+        <div class="element"> <a href="git.html">LINUX</a> </div>
+    </nav>
+</div>
